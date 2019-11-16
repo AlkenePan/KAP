f23115303e89c6acad638c1ff1c44e058918c840
Alkene Pan <alkenepan@gmail.com>
2019-11-16 19:23:48 +0800

    alert client added

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,12 +1,13 @@
 package client
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"time"
 )
-var httpClient = &http.Client{Timeout: 10 * time.Second}
 
+var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 func FetchPubKey(host string, appid string, target interface{}) error {
 	resp, err := httpClient.Get(
@@ -19,7 +20,6 @@ func FetchPubKey(host string, appid string, target interface{}) error {
 	return json.NewDecoder(resp.Body).Decode(target)
 }
 
-
 func FetchPriKey(host string, appid string, target interface{}) error {
 	resp, err := httpClient.Get(
 		"http://" + host + "/key/pri/" + appid,
@@ -30,7 +30,6 @@ func FetchPriKey(host string, appid string, target interface{}) error {
 	defer resp.Body.Close()
 	return json.NewDecoder(resp.Body).Decode(target)
 }
-
 
 func FetchAppInfo(host string, appid string, target interface{}) error {
 	resp, err := httpClient.Get(
@@ -43,4 +42,15 @@ func FetchAppInfo(host string, appid string, target interface{}) error {
 	return json.NewDecoder(resp.Body).Decode(target)
 }
 
-func NewAlert()
+func NewAlert(host string, appid string, target interface{}) error {
+	jsonStr, err := json.Marshal(target)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := httpClient.Post(
+		"http://"+host+"/alert/new",
+		"application/json",
+		bytes.NewBuffer(jsonStr))
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(target)
+}
