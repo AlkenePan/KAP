2ff097a744f9a5ad53befe1825c64f96e6116557
Alkene Pan <alkenepan@gmail.com>
2019-11-16 11:55:41 +0800

    argv & envv added in App

--- pkg/crypto/crypto_test.go
+++ pkg/crypto/crypto_test.go
@@ -1,14 +1,15 @@
 package crypto
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCrypto(t *testing.T) {
 	//fmt.Println("test RSA")
 	priv, pub := GenerateKeyPair(2048)
-	//fmt.Println("pub key: ", string(PublicKeyToBytes(pub)))
-	//fmt.Println("priv key: ", string(PrivateKeyToBytes(priv)))
+	fmt.Println("pub key: ", string(PublicKeyToBytes(pub)))
+	fmt.Println("priv key: ", string(PrivateKeyToBytes(priv)))
 	plaintext := []byte("gaba gaba gaba hey")
 	plaintextBase64Encoded := BytesBase64Encode(plaintext)
 	ciphertext := EncryptWithPublicKey(plaintextBase64Encoded, pub)
@@ -18,9 +19,9 @@ func TestCrypto(t *testing.T) {
 	decryptedtext := DecryptWithPrivateKey(ciphertextBase64Decoded, priv)
 	decryptedtextBase64Decoded := BytesBase64Decode(decryptedtext)
 
-	//fmt.Println(string(plaintext))
-	//fmt.Println(string(ciphertextBase64Encoded))
-	//fmt.Println(string(decryptedtextBase64Decoded))
+	fmt.Println(string(plaintext))
+	fmt.Println(string(ciphertextBase64Encoded))
+	fmt.Println(string(decryptedtextBase64Decoded))
 	if string(plaintext) != string(decryptedtextBase64Decoded) {
 		t.Error(
 			"expected", plaintext,
